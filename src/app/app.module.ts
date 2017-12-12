5ec0ad899cfcff5d48a0440554159b19da0e7c8f
Jimmy Kasprzak <jimmy.kasprzak@afp.com>
2017-12-12 14:34:04 +0100

    Test hello-world-lib.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HelloWorldModule } from 'hello-world-lib/dist/hello-world.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HelloWorldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
